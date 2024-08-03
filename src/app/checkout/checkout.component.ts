38fa4c975736e05f9bb20d62e12bc85bb6284450
mohamadshawabkeh <mohamadshawabkeh45@outlook.com>
2024-08-04 01:09:42 +0300

    creating checkout page and fix issues

---
+++ src/app/checkout/checkout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.scss'
+})
+export class CheckoutComponent {
+  coupon:string="assets/icons/checkout/coupon.svg";
+  offer:string="assets/icons/checkout/offer.svg";
+
+}
